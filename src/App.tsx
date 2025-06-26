f9fee8a15b4eb2253c725fcf7fe9b00c5c74ff5b
Prakash Kumar Mali <prakashkumarmali@Prakashs-MacBook-Pro.local>
2025-06-26 16:05:58 +0530

    remove default customer

--- src/App.tsx
+++ src/App.tsx
@@ -36,11 +36,7 @@ function App() {
       {
         id: 'customers',
         title: 'Customers',
-        people: [
-          { id: '1', name: 'John Doe' },
-          { id: '2', name: 'Jane Smith' },
-          { id: '3', name: 'Alex Johnson' },
-        ],
+        people: [],
       },
     ];
   });
